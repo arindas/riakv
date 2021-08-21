42b29dfece3fe418f08365cd832e5cf1c7d3f48c
Arindam Das <dasarindam.mails@gmail.com>
2021-08-21 22:54:32 +0530

    Completed cli implementation.

--- src/riakv_mem.rs
+++ src/riakv_mem.rs
@@ -1,5 +1,6 @@
+use libriakv::RiaKV;
 
-#[cfg(target_os="windows")]
+#[cfg(target_os = "windows")]
 const USAGE: &str = "
 Usage:
     riakv_mem.exe FILE get KEY
@@ -8,7 +9,7 @@ Usage:
     riakv_mem.exe FILE update KEY VALUE
 ";
 
-#[cfg(target_os="linux")]
+#[cfg(target_os = "linux")]
 const USAGE: &str = "
 Usage:
     riakv_mem FILE get KEY
@@ -17,21 +18,35 @@ Usage:
     riakv_mem FILE update KEY VALUE
 ";
 
-
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     let fname = args.get(1).expect(&USAGE);
 
     let action = args.get(2).expect(&USAGE).as_ref();
+    let key = args.get(3).expect(&USAGE).as_ref();
+    let maybe_value = args.get(4);
 
-    let _path = std::path::Path::new(fname); 
-
+    let path = std::path::Path::new(fname);
+    let mut store = RiaKV::open_from_file_at_path(path).expect("unable to open file");
+    store.load().expect("unable to load data");
 
     match action {
-        "get" => {},
-        "delete" => {},
-        "insert" => {},
-        "update" => {},
-        _ => {}
+        "get" => match store.get(key).unwrap() {
+            None => eprintln!("{:?} not found", key),
+            Some(value) => println!("{:?}", value),
+        },
+
+        "delete" => store.delete(key).unwrap(),
+
+        "insert" => {
+            let value = maybe_value.expect(&USAGE).as_ref();
+            store.insert(key, value).unwrap()
+        }
+
+        "update" => {
+            let value = maybe_value.expect(&USAGE).as_ref();
+            store.update(key, value).unwrap()
+        }
+        _ => eprintln!("{}", &USAGE),
     }
 }
