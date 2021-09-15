4108a583d20a433769e0d24e8f97c483d506d2bd
arindas <arindas@users.noreply.github.com>
2021-09-15 08:04:08 +0000

    deploy: 8b7b011c23703fb6b532767921b2fc798691d3e2

--- implementors/serde/de/trait.Deserialize.js
+++ implementors/serde/de/trait.Deserialize.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["libriakv"] = [{"text":"impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.128/serde/de/trait.Deserialize.html\" title=\"trait serde::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"struct\" href=\"libriakv/struct.KeyValuePair.html\" title=\"struct libriakv::KeyValuePair\">KeyValuePair</a>","synthetic":false,"types":["libriakv::KeyValuePair"]}];
+implementors["libriakv"] = [{"text":"impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.130/serde/de/trait.Deserialize.html\" title=\"trait serde::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"struct\" href=\"libriakv/struct.KeyValuePair.html\" title=\"struct libriakv::KeyValuePair\">KeyValuePair</a>","synthetic":false,"types":["libriakv::KeyValuePair"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- implementors/serde/ser/trait.Serialize.js
+++ implementors/serde/ser/trait.Serialize.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["libriakv"] = [{"text":"impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.128/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"libriakv/struct.KeyValuePair.html\" title=\"struct libriakv::KeyValuePair\">KeyValuePair</a>","synthetic":false,"types":["libriakv::KeyValuePair"]}];
+implementors["libriakv"] = [{"text":"impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.130/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"libriakv/struct.KeyValuePair.html\" title=\"struct libriakv::KeyValuePair\">KeyValuePair</a>","synthetic":false,"types":["libriakv::KeyValuePair"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
